d987565461bf357f2b10c48711ca8f6240284764
qwertiek <119134870+qwertiek@users.noreply.github.com>
2024-12-07 00:46:42 +0300

    Update +page.svelte

--- frontend/src/routes/code-input/+page.svelte
+++ frontend/src/routes/code-input/+page.svelte
@@ -9,7 +9,7 @@
         [key: string]: string[]; // Ключ — строка, значение — массив строк (файлов)
     };
 
-    let files: string[] = ["file.txt", "main.py"]; // Список файлов
+    let files: string[] = ["file.txt", "main.py"];
     let folders: Folders = {}; // Инициализация папок
     let selectedFile: string = "main.py";
     let showMenu: boolean = false;
@@ -44,62 +44,102 @@
         alert(`Selected option: ${option}`);
         closeMenu();
     };
+
+    // для верхнего меню
+    let projectName = "the name of project";
+    let lastModified = "5 days ago";
+    let size = "203.57 MB";
+    let tabs = ["main.py", "script.js", "script.js", "script.js"];
+    let searchQuery = "";
 </script>
 
 
 <div class="container">
     <div class="header">
-        Верхний блок (Header)
+          <!-- Левая секция -->
+        <div class="left-section">
+            <div class="icons">
+                <button><img src="./images/icon-sidebar.svg" alt=""></button>
+                <form action="http://localhost:5173/main" target="_blank">
+                    <button><img src="./images/icon-home.svg" alt=""></button>
+                </form>
+            </div>
+            <div style="margin-left: 1.3rem;">{projectName}</div>
+            <div class="prj-settings">{lastModified} • {size}</div>
+        </div>
+
+        <div class="search-container">
+            <img src="./images/icon-search.svg" alt="">
+            <input
+            class="search-input"
+            type="text"
+            placeholder="Найти команду..."
+            bind:value={searchQuery}
+            />
+        </div>
+
+        <div class="right-section">
+            <button class="run-button">Run</button>
+            <button class="invite-button"><img src="./images/icon-invite.svg" alt="">Пригласить</button>
+            <button><img src="./images/icon-plus.svg" alt=""></button>
+            <button><img src="./images/icon-noti.svg" alt=""></button>
+            <button><img src="./images/icon-more.svg" alt=""></button>
+            <div class="avatar"></div>
+        </div>
     </div>
+
     <div class="content">
         <div class="sidebar">
-            <div class="sidebar">
-                <div class="sidebar-header">
-                  <h3>Files</h3>
-                  <div >
-                    <button on:click={createFile}><img src="./images/icon-file-add.svg" alt=""></button>
-                    <button on:click={createFolder}><img src="./images/icon-folder-add.svg" alt=""></button>
-                    <button class="menu-button" on:click={toggleMenu}><img src="./images/3-poits.svg" alt=""></button>
-                    {#if showMenu}
-                      <!-- svelte-ignore a11y_no_static_element_interactions -->
-                      <div class="menu" on:mouseleave={closeMenu}>
-                        <div on:click={() => handleMenuOption("Option 1")}>Option 1</div>
-                        <div on:click={() => handleMenuOption("Option 2")}>Option 2</div>
-                        <div on:click={() => handleMenuOption("Option 3")}>Option 3</div>
-                      </div>
-                    {/if}
-                  </div>
+            <div class="sidebar-header">
+                <h3>Files</h3>
+                <div >
+                <button on:click={createFile}><img src="./images/icon-file-add.svg" alt=""></button>
+                <button on:click={createFolder}><img src="./images/icon-folder-add.svg" alt=""></button>
+                <button class="menu-button" on:click={toggleMenu}><img src="./images/3-poits.svg" alt=""></button>
+                {#if showMenu}
+                    <!-- svelte-ignore a11y_no_static_element_interactions -->
+                    <div class="menu" on:mouseleave={closeMenu}>
+                    <div on:click={() => handleMenuOption("Option 1")}>Option 1</div>
+                    <div on:click={() => handleMenuOption("Option 2")}>Option 2</div>
+                    <div on:click={() => handleMenuOption("Option 3")}>Option 3</div>
+                    </div>
+                {/if}
                 </div>
-            
-                <ul class="file-list">
-                    {#each files as file}
-                        <li
-                        class:selected={selectedFile === file}
-                        on:click={() => selectFile(file)}
-                        >
-                        {file}
-                        </li>
-                    {/each}
-            
-                    {#each Object.keys(folders) as folder}
-                        <div class="folder">
-                            {folder}
-                            <ul class="folder-files">
-                                {#each folders[folder] as file}
-                                <li
-                                    class:selected={selectedFile === file}
-                                    on:click={() => selectFile(file)}
-                                >
-                                    {file}
-                                </li>
-                                {/each}
-                            </ul>
-                        </div>
-                    {/each}
-                </ul>
             </div>
+        
+            <ul class="file-list">
+                {#each files as file}
+                    <li
+                    class:selected={selectedFile === file}
+                    on:click={() => selectFile(file)}
+                    >
+                    {file}
+                    </li>
+                {/each}
+        
+                {#each Object.keys(folders) as folder}
+                    <div class="folder">
+                        {folder}
+                        <ul class="folder-files">
+                            {#each folders[folder] as file}
+                            <li
+                                class:selected={selectedFile === file}
+                                on:click={() => selectFile(file)}
+                            >
+                                {file}
+                            </li>
+                            {/each}
+                        </ul>
+                    </div>
+                {/each}
+            </ul>
         </div>
         <div class="main">
+            <div class="tabs">
+                {#each tabs as tab}
+                    <div class="tab">{tab}</div>
+                {/each}
+                </div>
             <div class="code-input">
                 <CodeMirror
                 bind:value
@@ -123,19 +163,120 @@
         height: 100vh;
     }
 
+    /* верхний блок */
     .header {
         width: 100%;
+        max-height: 5.5rem;
         text-align: center;
         padding: 20px;
         box-sizing: border-box;
         border-bottom: 1px solid #444;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.5rem 1rem;
     }
 
+    .left-section {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .icons {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1.2rem;        
+    }
+
+    .icons button {
+        border-color: transparent;
+        cursor: pointer;
+    }
+
+    .icons img {width: 1.5rem; height: 1.5rem;}
+
+    .prj-settings {color: #7E7E7E;}
+
+    .search-container {
+        display: flex;
+        justify-content: center;
+        background: transparent;
+        border-radius: 10px;
+        padding: 0.3rem;
+        padding-left: 1.3rem;
+        flex-grow: 1;
+        max-width: 400px;
+        border: 1px solid  #7E7E7E;
+        margin: 0.7rem 1rem;
+    }
+
+    .search-input {
+        flex-grow: 1;
+        border: none;
+        outline: none;
+        padding: 0.5rem;
+        color: #ffffff;
+    }
+
+    .search-input::placeholder {
+        color: #7E7E7E;
+    }
+
+    .right-section {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .right-section button {
+        border-radius: 10px;
+        border-color: transparent;
+        cursor: pointer;
+        color: #ffffff;
+    }
+
+    .run-button {
+        padding: 0.7rem 1rem;
+        background-color: #218B01;
+        border: none;
+        cursor: pointer;
+        width: 5.5rem;
+    }
+
+    .run-button:hover {
+        background-color: #1a5d05;
+    }
+
+    .invite-button {
+        background-color: #162832;
+        padding: 0.5rem 0.75rem;
+        width: 8rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    .invite-button img {
+        background: transparent;
+    }
+
+    .avatar {
+        width: 2.5rem;
+        height: 2.5rem;
+        background-color: #444;
+        border-radius: 50%;
+    }
+
+    
     .content {
         display: flex;
         flex: 1;
     }
 
+    /* боковой блок */
     .sidebar {
         width: 17.5rem;
         padding: 20px;
@@ -196,9 +337,24 @@
         margin-left: 10px;
     }
 
+    /* основной блок */
     .main {
         flex: 1;
         padding: 20px;
         box-sizing: border-box;
+        background-color: #162832;
     }
+
+    .tabs {
+        display: flex;
+        gap: 0.5rem;
+    }
+
+    .tab {
+        padding: 0.25rem 0.5rem;
+        background-color: #333;
+        border: 1px solid #444;
+        border-radius: 3px;
+    }
+
 </style>
