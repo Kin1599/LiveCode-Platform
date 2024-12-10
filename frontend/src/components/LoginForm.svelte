ae790b781eb0b8afb6d3f5e90e326217efb9acd1
Kin <dkonopliannikov05@gmail.com>
2024-12-10 20:33:02 +0300

    split the login page into components

---
+++ frontend/src/components/LoginForm.svelte
@@ -0,0 +1,132 @@
+<script>
+    import SendServer from "../api/api";
+  
+    let email = "";
+    let password = "";
+    let message = "";
+  
+    const handleLogin = async () => {
+        message = "";
+        try {
+            const response = await SendServer.login(email, password);
+            console.log(response);
+            if (response.status === 200) {
+                localStorage.setItem("token", response.data.token);
+                message = "Успешный вход";
+                window.location.assign("/#");
+            } else {
+                message = "Ошибка при входе: " + response.statusText;
+            }
+        } catch (error) {
+            console.error("Login error:", error);
+            message = "Произошла ошибка при входе";
+        }
+    };
+</script>
+  
+<form>
+    <div class="form-widget">
+        <h2 class="header" style="margin-bottom: 0.5rem;">Войдите в аккаунт</h2>
+        <div class="form-group">
+            <input
+                required
+                id="email"
+                class="form-control"
+                type="email"
+                placeholder="Почта или имя пользователя"
+                bind:value={email}
+            />
+        </div>
+        <div class="form-group">
+            <input
+                required
+                id="password"
+                class="form-control"
+                type="password"
+                placeholder="Пароль"
+                bind:value={password}
+            />
+        </div>
+        <div>
+            <button type="button" on:click={handleLogin}>Войти</button>
+        </div>
+      
+        <div class="link">
+            <a href="/forgot-pass">Забыли пароль?</a>
+        </div>
+
+        <div class="register">
+            <div>Нет аккаунта? <span class="register-link">Зарегайтесь!</span></div>
+        </div>
+  
+        {#if message}
+            <p>{message}</p>
+        {/if}
+    </div>
+</form>
+  
+<style>
+    .form-widget {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        padding: 3rem;
+        text-align: center;
+        margin-top: 8rem;
+        width: 450px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+  
+    input {
+        width: 100%;
+        height: 50px;
+        background-color: #ffffff;
+        border-radius: 10px;
+        position: relative;
+        padding-left: 1rem;
+    }
+  
+    button {
+        color: white;
+        background-color: #ff7b00;
+        border-style: solid;
+        width: 104%;
+        height: 3.5rem;
+        border-radius: 10px;
+        cursor: pointer;
+        font-weight: 700;
+        font-size: 1.2rem;
+    }
+  
+    button:hover {
+        background: transparent;
+        border-width: 2px;
+        border-color: #ff7b00;
+        color: #ff7b00;
+    }
+  
+    .link {
+        margin-top: 0.5rem;
+        align-self: flex-start;
+    }
+  
+    .link a {
+        color: #ffffff;
+        text-decoration: none;
+    }
+  
+    .link a:hover {
+        color: #ff7b00;
+    }
+
+    .register{
+        text-align: left;
+    }
+
+    .register-link{
+        color: #ff7b00;
+        cursor: pointer;
+    }
+</style>
+  
---
+++ frontend/src/components/SocialLoginButtons.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+
+    type Provider = 'Google' | 'GitHub' | 'Apple';
+
+    // Логика для обработки кликов на кнопки (если потребуется)
+    const handleSocialLogin = (provider: Provider) => {
+      console.log(`Logging in with ${provider}`);
+    };
+  </script>
+  
+  <div class="but-log">
+    <button class="button-login" on:click={() => handleSocialLogin('Google')}>
+      <img src="./images/Google Icon.svg" alt="Google" height="22" width="22" />
+      Google
+    </button>
+    <button class="button-login" on:click={() => handleSocialLogin('GitHub')}>
+      <img src="./images/Github Icon.svg" alt="GitHub" height="27" width="27" />
+      GitHub
+    </button>
+    <button class="button-login" on:click={() => handleSocialLogin('Apple')}>
+      <img src="./images/Apple Icon.svg" alt="Apple" height="25" width="25" />
+      Apple
+    </button>
+  </div>
+  
+  <style>
+    .but-log {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      padding: 1rem;
+      text-align: center;
+      align-items: center;
+      width: 450px;
+      margin-left: auto;
+      margin-right: auto;
+      padding-right: 0.2rem;
+    }
+  
+    .button-login {
+      color: black;
+      background-color: #ffffff;
+      width: 104%;
+      height: 3.5rem;
+      border-radius: 10px;
+      cursor: pointer;
+      font-size: 20px;
+      border-style: none;
+      margin: 0.1rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      opacity: 1;
+    }
+  
+    .button-login:hover {
+      box-shadow: 0px 0px 15px 1px #ff7b00;
+    }
+  
+    .button-login img {
+      align-items: center;
+      margin: 3px;
+      background-color: transparent;
+      margin-right: 7px;
+    }
+  </style>
+  
