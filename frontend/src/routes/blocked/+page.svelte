ff0d2804399f29603d76c943a866c3434c8db3e3
Kin <dkonopliannikov05@gmail.com>
2024-12-12 01:09:09 +0300

    upd blocked page

--- frontend/src/routes/blocked/+page.svelte
+++ frontend/src/routes/blocked/+page.svelte
@@ -2,19 +2,69 @@
    
 </script>
   
-<div class="container">
-  <h1>Вы были заблокированы</h1>
-  <p>Ваш IP-адрес был заблокирован в этой сессии. Пожалуйста, свяжитесь с администратором, если считаете, что это ошибка.</p>
+<div class="blocked">
+  <div class="blocked-content">
+    <h1 class="blocked-title">Упс...</h1>
+    <div class="blocked-description">
+      <p class="description">Кажется, вас кикнули...(</p>
+      <p class="description">Чтобы вернуться обратно, свяжитесь с владельцем сессии</p>
+    </div>
+    <button class="blocked-button" onclick={() => window.location.assign("/")}>Вернуться на главную</button>
+  </div>
+  <img class="blocked-image" src="./images/blocked.svg" alt="blocked">
 </div>
 
 <style>
-  .container {
+  .blocked {
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     height: 100vh;
     text-align: center;
-    background-color: #f8f9fa;
+    position: relative;
+  }
+
+  .blocked-image{
+    position: absolute;
+    left: 0;
+    top: 50px;
+    z-index: -1;
+  }
+
+  .blocked-content > *:not(:last-child){
+    margin-bottom: 50px;
+  }
+
+  .blocked-title{
+    font-size: 96px;
+    font-weight: 500;
+    line-height: 112.5px;
+    color: #FF7B00;
+  }
+
+  .blocked-description{
+    font-size: 36px;
+    font-weight: 300;
+    line-height: 42.19px;
+    width: 635px;
+  }
+
+  .blocked-description > *:not(:last-child){
+    margin-bottom: 25px;
+  }
+
+  .blocked-button{
+    background-color: #162832;
+    border-radius: 10px;
+    font-size: 32px;
+    line-height: 37.5px;
+    font-weight: 500;
+    color: #fff;
+    border: none;
+    width: 100%;
+    height: 88px;
+    cursor: pointer;
   }
 </style>
   
